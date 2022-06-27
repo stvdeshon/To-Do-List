96a3bb6506bcf8243c3f4b7482722d0a32c4fe5f
stvdeshon <93623464+stvdeshon@users.noreply.github.com>
2022-06-27 15:06:31 -0600

    Add classes for lists and list items

---
+++ src/to-do-items.js
@@ -0,0 +1,50 @@
+/*This module is for creating new Do Do List items*/
+
+export default class Task {
+    constructor(title, description, dueDate, priority) {
+        this._title = title;
+        this._description = description;
+        this._priority = priority;
+        this._dueDate = dueDate;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set priority(value) {
+        this._priority = value;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set dueDate(value) {
+        this._dueDate = value;
+    }
+}
+
+// Message to myself:
+// So far, I only have getters and setters for the main values of the items however
+// I may have to alter these as the project continues as I have added no serious
+// functionality to the methods themselves. I also do not yet know how the date
+// and priority mechanisms will ultimately function. I have created no other methods
+// because unlike the projects themselves, the items should have no elements that
+// would ever need to be entirely deleted or moved elsewhere.
---
+++ src/to-do-lists.js
@@ -0,0 +1,37 @@
+/*This module is for creating new user projects on the To Do List*/
+
+export default class ToDoList {
+    constructor(title){
+        this._title = title;
+        this._toDoItems = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        this._title = value
+    }
+
+    get toDoItems() {
+        return this._toDoItems;
+    }
+
+    addToDoItems(item) {
+        this.toDoItems.push(item);
+    }
+
+    removeToDoItems() {
+        //remove an item from the array
+    }
+}
+
+// Message to me later as to what I may change in this module:
+// For now, this only sets a title and a list of constituent list items
+// although I may change the addToDoItems parameter to use spread syntax
+// if at some future point it proves necessary. This module has no DOM 
+// methods but I should be able to use the getters and setters in other
+// modules to that effect. There might be a need for an 'is complete'
+// method either in this class or the to do item class to mark
+// individual projects but that all remains to be seen.
