53962bc9da28b55e53d561f814e4ac116526267b
stvdeshon <93623464+stvdeshon@users.noreply.github.com>
2022-06-28 11:28:55 -0600

    Add files via upload

---
+++ src/DOMfunctions.js
@@ -0,0 +1,12 @@
+import Plan from './to-do-lists';
+import PlanItem from './to-do-items';
+
+// Add character limit to PlanItem titles
+
+// Delete buttons call removal method and deletes from DOM
+
+// The add plan button calls up a form, submitting the form instantiates a new Plan object and populates the sidebar with it
+
+// The newly created 'plan' buttons on the sidebar will populate the display with the corresponding object's data 
+// That object's display will have an 'add item' button that will open a form that submits and creates a new item object
+// The subsequent item button will populate the display and have a few functional buttons to edit the corresponding item object and change the DOM values
